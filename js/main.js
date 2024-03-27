f49c16f54a8726ca65494809450201fc2e9fa377
katrina-macadams <katrinamacadams@gmail.com>
2024-03-27 19:58:27 -0400

    started audio functionality
    Input the audio functionality into the HTML, and started creating variables, event listeners and some functions in JS

--- js/main.js
+++ js/main.js
@@ -6,10 +6,19 @@ let soundBoard = document.querySelector(".sound_boards"), //puzzleBoard
     soundContainer = document.querySelector(".sound_cont"), //pieceContainer
     slotZone = document.querySelectorAll('.sand_box'), //dropZone
 
+    soundIcons = document.querySelectorAll("#sounds img"),
+    theAudioEl = document.querySelector('#audioEl'),
+    pauseButton = document.querySelector('#pause'),
+    resetButton = document.querySelector('#reset'),
+    helpButton = document.querySelector('#help'),
+
 
     draggedSound; //draggedPiece
+
     
 //functions
+
+// drag n drop
 
 function resetSounds() {
     slotZone.forEach (zone => {
@@ -40,6 +49,29 @@ function handleDrop(e) {
     this.appendChild(draggedSound); 
 }
 
+// audio 
+
+function loadAudio() {
+    let currentSrc = `audio/${this.dataset.trackref}.mp3`;
+    theAudioEl.src = currentSrc;    
+    theAudioEl.load();
+
+    playAudio();
+}
+
+function playAudio() { 
+    theAudioEl.play(); 
+}
+function restartAudio() { 
+    theAudioEl.currentTime = 0; 
+    playAudio(); 
+}
+
+function pauseAudio() { 
+    theAudioEl.pause(); 
+}
+
+
 
 
 //eventlisteners
@@ -49,3 +81,5 @@ seaSound.forEach(sound => sound.addEventListener ("dragstart", handleStartDrag))
 slotZone.forEach(zone => zone.addEventListener("dragover", handleDragOver)); 
 
 slotZone.forEach(zone => zone.addEventListener("drop", handleDrop)); 
+
+soundIcons.forEach(icons => icons.addEventListener('click', loadAudio)); 
